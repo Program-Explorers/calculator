0e52b34af1e7b8237e4b3d1ee220429f7e7fada3
Programlog <varunk3249@gmail.com>
2020-07-22 14:15:45 -0400

    made label and few buttons

--- calc.py
+++ calc.py
@@ -1,2 +1,32 @@
-# Calculator
 import tkinter as tk
+
+root = tk.Tk()
+
+height = 300
+width = 225
+
+root.title('Calculator')
+
+canvas = tk.Canvas(root, height=height, width=width)
+canvas.pack()
+
+in_output = '0'
+
+frame = tk.Frame(root, bg='#2b2929')
+frame.place(relheight=1, relwidth=1)
+
+out_var = tk.StringVar()
+output = tk.Label(frame, textvariable=out_var, bg='#2b2929', fg='#ffffff', font="Calibri 40")
+output.grid()
+out_var.set('0')
+
+clear = tk.Button(frame, text='Clear')
+clear.grid(row=1, column=0, columnspan=2)
+
+percent = tk.Button(frame, text='%')
+percent.grid(row=1, column=2)
+
+divide = tk.Button(frame, text='÷')
+divide.grid(row=1, column=3)
+
+root.mainloop()
