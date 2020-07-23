1bce0cbdae0552be0b0c64e03101a747d4714754
Programlog <varunk3249@gmail.com>
2020-07-23 14:10:16 -0400

    made gui look better with grid method

--- calc.py
+++ calc.py
@@ -2,8 +2,9 @@ import tkinter as tk
 
 root = tk.Tk()
 
-height = 300
-width = 225
+height = 297
+width = 250
+font_type = 'Calibri 25'
 
 root.title('Calculator')
 
@@ -20,8 +21,8 @@ output = tk.Label(frame, textvariable=out_var, bg='#2b2929', fg='#ffffff', font=
 output.grid()
 out_var.set('0')
 
-clear = tk.Button(frame, text='Clear')
-clear.grid(row=1, column=0, columnspan=2)
+clear = tk.Button(frame, text='Clear', height=1, width=8, font='Calibri 25')
+clear.grid(row=1, column=0, columnspan=2, sticky='w')
 
 percent = tk.Button(frame, text='%')
 percent.grid(row=1, column=2)
@@ -29,11 +30,11 @@ percent.grid(row=1, column=2)
 divide = tk.Button(frame, text='÷')
 divide.grid(row=1, column=3)
 
-num_7 = tk.Button(frame, text='7')
-num_7.grid(row=2,column=0)
+num_7 = tk.Button(frame, text='7', height=2, width=4, font='Calibri 23')
+num_7.grid(row=2, column=0, sticky='w', padx=0, pady=1)
 
-num_8 = tk.Button(frame, text='8')
-num_8.grid(row=2,column=1)
+num_8 = tk.Button(frame, text='8', height=2, width=4, font='Calibri 23')
+num_8.grid(row=2, column=1, sticky='w', padx=1, pady=1)
 
 num_9 = tk.Button(frame, text='9')
 num_9.grid(row=2,column=2)
@@ -41,11 +42,11 @@ num_9.grid(row=2,column=2)
 multiply = tk.Button(frame, text='×')
 multiply.grid(row=2,column=3)
 
-num_4 = tk.Button(frame, text='4')
-num_4.grid(row=3,column=0)
+num_4 = tk.Button(frame, text='4', height=2, width=4, font='Calibri 23')
+num_4.grid(row=3, column=0, sticky='w', pady=1)
 
-num_5 = tk.Button(frame, text='5')
-num_5.grid(row=3, column=1)
+num_5 = tk.Button(frame, text='5', height=2, width=4, font='Calibri 23')
+num_5.grid(row=3, column=1, sticky='w', padx=1, pady=1)
 
 num_6 = tk.Button(frame, text='6')
 num_6.grid(row=3, column=2)
@@ -53,25 +54,26 @@ num_6.grid(row=3, column=2)
 minus = tk.Button(frame, text='-')
 minus.grid(row=3, column=3)
 
-num_1 = tk.Button(frame, text='1')
-num_1.grid(row=4, column=0)
+num_1 = tk.Button(frame, text='1', height=2, width=4, font='Calibri 23')
+num_1.grid(row=4, column=0, sticky='w', pady=1)
 
-num_2 = tk.Button(frame, text='2')
-num_2.grid(row=4, column=1)
+num_2 = tk.Button(frame, text='2', height=2, width=4, font='Calibri 23')
+num_2.grid(row=4, column=1, sticky='w', padx=1, pady=1)
 
-num_3 = tk.Button(frame, text='3')
-num_3.grid(row=4, column=2)
+num_3 = tk.Button(frame, text='3', height=2, width=4, font='Calibri 23')
+num_3.grid(row=4, column=2, sticky='w', pady=1)
 
 plus = tk.Button(frame, text='+')
 plus.grid(row=4, column=3)
 
-num_0 = tk.Button(frame, text='0')
-num_0.grid(row=5, column=0)
+num_0 = tk.Button(frame, text='0', height=1, width=8, font='Calibri 25')
+num_0.grid(row=5, column=0, columnspan=2, sticky='w', pady=1)
 
 decimal = tk.Button(frame, text='.')
-decimal.grid(row=5, column=1)
+decimal.grid(row=5, column=2)
 
 equals = tk.Button(frame, text='=')
-equals.grid(row=5, column=2)
+equals.grid(row=5, column=3)
 
 root.mainloop()
+
