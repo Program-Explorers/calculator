c599874c18a96ce0c9c242f90b766138a2393d4e
Programlog <varunk3249@gmail.com>
2020-07-24 22:14:24 -0400

    Added support for operations. Still some bugs

--- calc_mac.py
+++ calc_mac.py
@@ -2,11 +2,19 @@ import tkinter as tk
 
 expression = ''
 
+operations = ['+', '×', '÷', '-']
+
 
 def press(num):
     global expression
-    expression = expression + str(num)
-    equation.set(expression)
+
+    if num == '+' or num == '-' or num == '×' or num == '÷':
+        equation.set(num)
+        expression = expression + str(num)
+    else:
+        expression = expression + str(num)
+        equation.set(expression)
+
     print(expression)
 
 
@@ -43,7 +51,7 @@ clear.grid(row=1, column=0, columnspan=2, padx=1, sticky='w', ipadx=7, pady=1)
 percent = tk.Button(frame, text='%', height=1, width=3, font='Calibri 25')
 percent.grid(row=1, column=2, ipadx=6, padx=1, sticky='w', pady=1)
 
-divide = tk.Button(frame, text='÷', height=1, width=3, font='Calibri 25')
+divide = tk.Button(frame, text='÷', height=1, width=3, font='Calibri 25', command=lambda: press('÷'))
 divide.grid(row=1, column=3, padx=1, sticky='w', pady=1, ipadx=6)
 
 num_7 = tk.Button(frame, text='7', height=2, width=4, font='Calibri 23', command=lambda: press(7))
@@ -55,7 +63,7 @@ num_8.grid(row=2, column=1, sticky='w', padx=1, pady=1)
 num_9 = tk.Button(frame, text='9', height=2, width=4, font='Calibri 23', command=lambda: press(9))
 num_9.grid(row=2, column=2, padx=1, pady=1, sticky='w')
 
-multiply = tk.Button(frame, text='×', height=2, width=4, font='Calibri 23')
+multiply = tk.Button(frame, text='×', height=2, width=4, font='Calibri 23', command=lambda: press('×'))
 multiply.grid(row=2, column=3, padx=1, pady=1, sticky='w')
 
 num_4 = tk.Button(frame, text='4', height=2, width=4, font='Calibri 23', command=lambda: press(4))
@@ -67,7 +75,7 @@ num_5.grid(row=3, column=1, sticky='w', padx=1, pady=1)
 num_6 = tk.Button(frame, text='6', height=2, width=4, font='Calibri 23', command=lambda: press(6))
 num_6.grid(row=3, column=2)
 
-minus = tk.Button(frame, text='-', height=2, width=4, font='Calibri 23')
+minus = tk.Button(frame, text='-', height=2, width=4, font='Calibri 23', command=lambda: press('-'))
 minus.grid(row=3, column=3, sticky='w', padx=1)
 
 num_1 = tk.Button(frame, text='1', height=2, width=4, font='Calibri 23', command=lambda: press(1))
@@ -79,13 +87,13 @@ num_2.grid(row=4, column=1, sticky='w', padx=1, pady=1)
 num_3 = tk.Button(frame, text='3', height=2, width=4, font='Calibri 23', command=lambda: press(3))
 num_3.grid(row=4, column=2, sticky='w', padx=1, pady=1)
 
-plus = tk.Button(frame, text='+', height=2, width=4, font='Calibri 23')
+plus = tk.Button(frame, text='+', height=2, width=4, font='Calibri 23', command=lambda: press('+'))
 plus.grid(row=4, column=3, padx=1, pady=1, sticky='w')
 
 num_0 = tk.Button(frame, text='0', height=1, width=7, font='Calibri 25', command=lambda: press(0))
 num_0.grid(row=5, column=0, columnspan=2, ipadx=7, sticky='w', pady=1, padx=1)
 
-decimal = tk.Button(frame, text='.', height=1, width=3, font='Calibri 25')
+decimal = tk.Button(frame, text='.', height=1, width=3, font='Calibri 25', command=lambda: press('.'))
 decimal.grid(row=5, column=2, sticky='w', padx=1, pady=1, ipadx=6)
 
 equals = tk.Button(frame, text='=', height=1, width=3, font='Calibri 25')
